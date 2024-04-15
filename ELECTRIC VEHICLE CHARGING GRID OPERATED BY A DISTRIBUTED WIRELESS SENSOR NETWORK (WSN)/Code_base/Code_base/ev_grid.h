8cb9311b187f63ec444ae4c7561b0fa1db90d736
1234ngochai <102779996+1234ngochai@users.noreply.github.com>
2024-04-15 21:09:15 +1000

    Delete ELECTRIC VEHICLE CHARGING GRID OPERATED BY A DISTRIBUTED WIRELESS SENSOR NETWORK (WSN) directory

--- ELECTRIC VEHICLE CHARGING GRID OPERATED BY A DISTRIBUTED WIRELESS SENSOR NETWORK (WSN)/Code_base/Code_base/ev_grid.h
+++
@@ -1,9 +0,0 @@
-#ifndef EV_GRID_H
-#define EV_GRID_H
-
-#include <mpi.h>
-
-void simulate_ev_node(MPI_Comm grid_comm);
-void send_report_to_base_station(int my_rank, int num_messages, int coords[2], int heavily_utilized_neighbors[4]);
-
-#endif
